13f610d574c34ee6382aa71d68d81c05213044f2
Calychas <kacper.lesniara@gmail.com>
2021-04-07 16:58:56 +0200

    initial commit

---
+++ settings.py
@@ -0,0 +1,7 @@
+from os.path import normpath, dirname, join
+
+PROJECT_DIR = normpath(dirname(__file__))
+
+DATA_DIR = join(PROJECT_DIR, "data")
+RAW_DATA_DIR = join(DATA_DIR, "raw")
+PROCESSED_DATA_DIR = join(DATA_DIR, "processed")
