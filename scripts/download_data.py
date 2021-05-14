fd21c59fbcfbcc154e6bfb5f1161e0c9e928f0ef
Calychas <kacper.lesniara@gmail.com>
2021-05-14 02:34:16 +0200

    add res 7, r&d

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -11,13 +11,12 @@ sys.path.append(str(PROJECT_DIR.joinpath("src").resolve()))
 from settings import *
 import scripts.generate_place as gp
 from src.tools.osmnx_utils import get_place_dir_name
-import time
 import warnings
 warnings.filterwarnings("ignore")
 
 network_type = "drive"
 cities = pd.read_csv(RAW_DATA_DIR.joinpath("cities.csv"))
-cities = cities[cities["city"] != "Poland"]  # TODO: remove
+cities = cities[cities["country"] == "Poland"]  # TODO: remove
 
 pbar_city = tqdm(cities.itertuples(), total=cities.shape[0])
 for row in pbar_city:
@@ -31,16 +30,19 @@ for row in pbar_city:
         pbar_commands.update()
 
         pbar_commands.set_description("Generate H3")
+        gp.h3.callback(GENERATED_DATA_DIR.joinpath(place_dir_name, "place.geojson"), GENERATED_DATA_DIR.joinpath(place_dir_name), 7, True)
         gp.h3.callback(GENERATED_DATA_DIR.joinpath(place_dir_name, "place.geojson"), GENERATED_DATA_DIR.joinpath(place_dir_name), 8, True)
         gp.h3.callback(GENERATED_DATA_DIR.joinpath(place_dir_name, "place.geojson"), GENERATED_DATA_DIR.joinpath(place_dir_name), 9, True)
         pbar_commands.update()
 
         pbar_commands.set_description("Assign H3")
+        gp.assign_h3.callback(GENERATED_DATA_DIR.joinpath(place_dir_name), network_type, 7, True)
         gp.assign_h3.callback(GENERATED_DATA_DIR.joinpath(place_dir_name), network_type, 8, True)
         gp.assign_h3.callback(GENERATED_DATA_DIR.joinpath(place_dir_name), network_type, 9, True)
         pbar_commands.update()
 
         pbar_commands.set_description("Generate features")
+        gp.features.callback(GENERATED_DATA_DIR.joinpath(place_dir_name), network_type, 7, True)
         gp.features.callback(GENERATED_DATA_DIR.joinpath(place_dir_name), network_type, 8, True)
         gp.features.callback(GENERATED_DATA_DIR.joinpath(place_dir_name), network_type, 9, True)
         pbar_commands.update()
