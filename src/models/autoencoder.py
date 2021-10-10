98c83cfbd2c16b6aafb7813a40dcd5f91a40ca1e
Calychas <kacper.lesniara@gmail.com>
2021-10-10 20:05:58 +0200

    add WIP graph autoencoder, prepare data for graph embedding

--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -1,7 +1,10 @@
 import torch
 from torch import nn
+from torch import optim
 import torch.nn.functional as F
 import pytorch_lightning as pl
+from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GAE
 
 
 class LitAutoEncoder(pl.LightningModule):
@@ -32,5 +35,47 @@ class LitAutoEncoder(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
+
+
+
+class GCNEncoder(pl.LightningModule):
+    def __init__(self, in_channels, out_channels) -> None:
+        super(GCNEncoder, self).__init__()
+        self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=True) # cached only for transductive learning
+        self.conv2 = GCNConv(2 * out_channels, out_channels, cached=True) # cached only for transductive learning
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        return self.conv2(x, edge_index)
+
+
+class LitGAE(pl.LightningModule):
+    def __init__(self, encoder, decoder=None) -> None:
+        super().__init__()
+        self.gae = GAE(encoder, decoder)
+
+    def encode(self, x, edge_index):
+        return self.gae.encode(x, edge_index)
+
+    def decode(self, z, sigmoid=True):
+        return self.gae.decode(z, sigmoid)
+
+    def forward(self, x, edge_index):
+        z = self.gae.encode(x, edge_index)
+        return z
+    
+    def training_step(self, batch, batch_idx):
+        x, edge_index = batch
+        z = self.encode(x, edge_index)
+        # x_hat = self.decode(z)
+        loss = self.gae.recon_loss(z, edge_index)
+
+        self.log('train_loss', loss, on_epoch=True, on_step=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+    
